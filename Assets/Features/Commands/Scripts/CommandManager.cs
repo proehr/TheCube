757ea8373fe87f8817c70f9309775f510aeab1bc
Nullformed <92059356+Nullformed@users.noreply.github.com>
2021-10-31 17:55:39 +0100

    31-basis-ui/15c-options: -Basic settings asset with saving and loading -Basic settings UI

---
+++ Assets/Features/Commands/Scripts/CommandManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandManager : MonoBehaviour
+{
+    private class WorkerCommand
+    {
+        
+    }
+}
