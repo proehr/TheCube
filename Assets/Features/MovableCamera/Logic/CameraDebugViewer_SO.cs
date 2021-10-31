ffc37465bcc397895ccf151b749340bc90914ddb
Gino Georgiev <Xyckno@gmail.com>
2021-10-31 18:20:38 +0100

    30-camera-controller/15e-simple-camera-movement (#11)
    * Added Camera feature folder, CameraPreset_SO.cs CameraSampleScene and CameraController.cs
    Added InputActionMap
    Added PlayerControls.cs
    Added Ignore Render Tag
    Adjusted EditorBuildSettings.asset
    
    * Moved Transparent.mat
    
    * Implemented basic CameraController
    
    * Added Comment for Debug Info
    Adjusted playerControls field name
    
    * Adjusted for Code-Review
    
    * Added Whitespace
    
    Co-authored-by: Georgiev <gino.georgiev@gmail.com>

---
+++ Assets/Features/MovableCamera/Logic/CameraDebugViewer_SO.cs
@@ -0,0 +1,38 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Features.MovableCamera.Logic
+{
+    [CreateAssetMenu(fileName = "NewCameraDebugViewer", menuName = "Camera/DebugViewer")]
+    public class CameraDebugViewer_SO : ScriptableObject
+    {
+        [ReadOnly] public Vector3 newPosition;
+        [ReadOnly] public Quaternion newRotation;
+        [ReadOnly] public Vector3 newZoom;
+
+        [Header("Current movementSpeed")]
+        [ReadOnly] public float movementSpeed;
+
+        [ReadOnly] public Vector2 movementInput;
+        [ReadOnly] public float shiftInput;
+        [ReadOnly] public float leftRotationInput;
+        [ReadOnly] public float rightRotationInput;
+        [ReadOnly] public float zoomInput;
+
+        public void ResetValues()
+        {
+            newPosition = new Vector3(0, 0, 0);
+            newRotation.x = 0;
+            newRotation.y = 0;
+            newRotation.z = 0;
+
+            zoomInput = 0;
+            movementInput = new Vector2(0, 0);
+        }
+
+        private void OnEnable()
+        {
+            ResetValues();
+        }
+    }
+}
---
+++ Assets/Features/MovableCamera/Logic/CameraPreset_SO.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Features.MovableCamera.Logic
+{
+    [CreateAssetMenu(fileName = "NewCameraPreset", menuName = "Camera/Preset")]
+    public class CameraPreset_SO : ScriptableObject
+    {
+        [Tooltip("Smoothness (Time it takes to Lerp between two values)")]
+        public float movementTime;
+        [Tooltip("Default Speed for actions")]
+        public float normalSpeed;
+        [Tooltip("Faster Speed when pressing LEFT-SHIFT")]
+        public float fastSpeed;
+        [Tooltip("Default Rotation Speed")]
+        public float rotationAmount;
+        [Tooltip("Default Zoom Speed")]
+        public Vector3 zoomAmount;
+    }
+}
