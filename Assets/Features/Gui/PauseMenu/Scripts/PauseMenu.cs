561ed0dcad9f0627afbc106db787fa12fb777ab5
Nullformed <92059356+Nullformed@users.noreply.github.com>
2021-11-13 15:40:02 +0100

    15a-pause-menu: -Added basic pause menu prefab -Added GamePausedState SO var, OnGamePausedChanged event -Removed SetTrue, SetFalse from BoolVariable, since Set method of base class is public --Fixed onValueChanged raised twice by BoolVariable methods --Added Toggle() method

---
+++ Assets/Features/Gui/PauseMenu/Scripts/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Features.Gui.PauseMenu
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        private bool isOpen;
+    
+        public void Toggle()
+        {
+            if (this.isOpen)
+            {
+                this.Close();
+            }
+            else
+            {
+                this.Open();
+            }
+        }
+
+        public void Close()
+        {
+            this.isOpen = false;
+            this.gameObject.SetActive(this.isOpen);
+        }
+
+        private void Open()
+        {
+            this.isOpen = true;
+            this.gameObject.SetActive(this.isOpen);
+        }
+
+        public void OnExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
