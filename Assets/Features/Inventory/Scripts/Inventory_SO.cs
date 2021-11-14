2b45b3fd032b6253b0780e508aaecb07322d1d6d
R. Zander <r-zander@users.noreply.github.com>
2021-11-14 18:45:37 +0100

    45-landing-pod/17x-create-inventory added inventory both as Scriptable Object and in HUD

---
+++ Assets/Features/Inventory/Scripts/Inventory_SO.cs
@@ -0,0 +1,14 @@
+﻿using DataStructures.Variables;
+using UnityEngine;
+
+namespace Features.Inventory.Scripts
+{
+    [CreateAssetMenu(fileName = "newInventory", menuName = "Inventory")]
+    public class Inventory_SO : ScriptableObject
+    {
+        [Tooltip("Amount of stored/available resources for worker production.")]
+        [SerializeField] private IntVariable resource;
+
+        public IntVariable Resource => resource;
+    }
+}
