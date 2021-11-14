3b6797bc17915de7e30f0ebe3d8278dcc07599b9
R. Zander <r-zander@users.noreply.github.com>
2021-11-14 19:35:21 +0100

    44 worker behaviour/17q generate navmesh on generated planet (#16)
    * 44-worker-behaviour/card/17q-generate-navmesh-on-generated-planet
    
    * Added prefabs to GameScene and removed debug rays.
    
    * Removed script that's not supposed to be in this branch
    
    * Replaced Debug.Log with Debug.LogWarning
    
    * Add all command posts to the same parent.

--- Assets/Features/WorkerAI/Command.cs
+++ Assets/Features/WorkerAI/Command.cs
@@ -7,10 +7,10 @@ namespace Features.WorkerAI
         public Vector3 location { get; private set; }
         private GameObject commandPost;
 
-        public Command(Vector3 location, GameObject commandPostPrefab, Vector3 planeNormal)
+        public Command(Vector3 location, GameObject commandPostPrefab, Transform commandPostsParent, Vector3 planeNormal)
         {
             this.location = location;
-            commandPost = Object.Instantiate(commandPostPrefab, location, Quaternion.identity);
+            commandPost = Object.Instantiate(commandPostPrefab, location, Quaternion.identity, commandPostsParent);
             commandPost.transform.up = planeNormal;
         }
 
