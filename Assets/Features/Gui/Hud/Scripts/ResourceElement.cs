d15603be21c3e1b604289eefb89f51b3833d0210
R. Zander <r-zander@users.noreply.github.com>
2021-11-14 18:45:37 +0100

    45-landing-pod/17x-create-inventory added inventory both as Scriptable Object and in HUD

---
+++ Assets/Features/Gui/Hud/Scripts/ResourceElement.cs
@@ -0,0 +1,27 @@
+﻿using DataStructures.Variables;
+using TMPro;
+using UnityEngine;
+
+namespace Features.Gui.Hud.Scripts
+{
+    public class ResourceElement : MonoBehaviour
+    {
+        [SerializeField] private IntVariable resource;
+        [SerializeField] private TMP_Text amountLabel;
+
+        private void Start()
+        {
+            UpdateLabel();
+        }
+
+        public void OnResourceAmountChanged()
+        {
+            UpdateLabel();
+        }
+
+        private void UpdateLabel()
+        {
+            amountLabel.text = resource.Get().ToString();
+        }
+    }
+}
