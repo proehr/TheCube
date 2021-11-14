a84b4e7b5beff9167098a7e457577ef5b65f1b58
R. Zander <r-zander@users.noreply.github.com>
2021-11-14 17:36:47 +0100

    45-landing-pod/17w-place-landing-pod

---
+++ Assets/Features/LandingPod/Scripts/LandingPod.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Features.LandingPod.Scripts
+{
+    public class LandingPod : MonoBehaviour
+    {
+        public void Init(GameObject surfaceCube)
+        {
+            var surfaceCubeTransform = surfaceCube.transform;
+            var thisTransform = this.transform;
+            thisTransform.position = surfaceCubeTransform.position +
+                                  surfaceCubeTransform.up * surfaceCubeTransform.localScale.y;
+            thisTransform.rotation = surfaceCubeTransform.rotation;
+        }
+    }
+}
