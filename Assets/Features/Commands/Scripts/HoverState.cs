a2d9e63758d9cf99db8bdfb79141875649b94ef9
Nullformed <92059356+Nullformed@users.noreply.github.com>
2021-11-28 03:52:18 +0100

    42-commands: * Improved hover state handling * Added dragging of excavate command by current hover state

---
+++ Assets/Features/Commands/Scripts/HoverState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class HoverState
+{
+    public enum State
+    {
+        None,
+        UI,
+        Cube,
+        CubeExcavate
+    }
+    private static State hoverState = State.None;
+    public static Action onHoverStateChangedEvent;
+    
+    public static State currentHoverState => hoverState;
+
+    public static void SetState(State newHoverState)
+    {
+        hoverState = newHoverState;
+        onHoverStateChangedEvent?.Invoke();
+    }
+}
