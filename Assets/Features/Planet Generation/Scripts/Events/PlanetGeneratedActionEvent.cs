c08e23dc77a7e53f7f305b0ee071ffdc6bac1b66
Nullformed <92059356+Nullformed@users.noreply.github.com>
2021-11-28 16:09:46 +0100

    refactor: * refactored base action event with parameter as abstract type * fixed overriding renderer and camera field names

--- Assets/Features/Planet Generation/Scripts/Events/PlanetGeneratedActionEvent.cs
+++ Assets/Features/Planet Generation/Scripts/Events/PlanetGeneratedActionEvent.cs
@@ -1,26 +1,10 @@
-using System;
+using DataStructures.Event;
 using UnityEngine;
 
 namespace Features.Planet_Generation.Scripts.Events
 {
     [CreateAssetMenu(fileName = "newPlanetGeneratedActionEvent", menuName = "Utils/ActionEvents/PlanetGenerated")]
-    public class PlanetGeneratedActionEvent : ScriptableObject
+    public class PlanetGeneratedActionEvent : ActionEventWithParameter<PlanetGenerator>
     {
-        Action<PlanetGenerator> listeners;
-
-        public void Raise(PlanetGenerator caller)
-        {
-            this.listeners?.Invoke(caller);
-        }
-
-        public void RegisterListener(Action<PlanetGenerator> listener)
-        {
-            this.listeners += listener;
-        }
-
-        public void UnregisterListener(Action<PlanetGenerator> listener)
-        {
-            this.listeners -= listener;
-        }
     }
 }
