8fae5c39ff302216ed2157fb0d7ef6e804a24d05
R. Zander <r-zander@users.noreply.github.com>
2021-11-28 18:37:41 +0100

    42-commands//19i-create-launch-command: Created launch command and let the launch pod manager handle the launch itself (not implemented yet)

---
+++ Assets/Features/LandingPod/Scripts/LaunchTriggeredActionEvent.cs
@@ -0,0 +1,15 @@
+﻿using DataStructures.Event;
+using UnityEngine;
+
+namespace Features.LandingPod.Scripts
+{
+    public class LaunchInformation
+    {
+        // TODO add whatever infos are required, e.g. next planet etc
+    }
+
+    [CreateAssetMenu(fileName = "OnLaunchTriggeredEvent", menuName = "Utils/ActionEvents/LaunchTriggered")]
+    public class LaunchTriggeredActionEvent : ActionEventWithParameter<LaunchInformation>
+    {
+    }
+}
