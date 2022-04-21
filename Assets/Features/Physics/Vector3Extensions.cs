ab40bb316deaab33b56709b659ce82802a50fd81
r-zander <r-zander@users.noreply.github.com>
2022-04-21 18:24:32 +0200

    1az-create-movement-script-for-90-degree-edges - Allows movement around outward facing edges

--- Assets/Features/Physics/Vector3Extensions.cs
+++ Assets/Features/Physics/Vector3Extensions.cs
@@ -8,5 +8,18 @@ namespace Features.Physics
         {
             return Mathf.Atan2(v.z, v.x);
         }
+
+        /// <summary>
+        /// Compares this vector with another vector. If the length difference is smaller or equal to the
+        /// acceptableDelta, this method returns true.
+        /// </summary>
+        public static bool EqualEnough(this Vector3 v, Vector3 other, float acceptableDelta)
+        {
+            float num1 = v.x - other.x;
+            float num2 = v.y - other.y;
+            float num3 = v.z - other.z;
+            return (double) num1 * (double) num1 + (double) num2 * (double) num2 + (double) num3 * (double) num3 <
+                   acceptableDelta * acceptableDelta;
+        }
     }
 }
