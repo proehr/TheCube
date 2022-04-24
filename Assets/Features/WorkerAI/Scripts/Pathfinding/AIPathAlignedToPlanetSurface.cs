3aff285f03f4675047fff776cb8ab02f275aafb7
r-zander <r-zander@users.noreply.github.com>
2022-04-24 18:52:34 +0200

    1az-create-movement-script-for-90-degree-edges - Allows movement around inward facing edges

--- Assets/Features/WorkerAI/Scripts/Pathfinding/AIPathAlignedToPlanetSurface.cs
+++ Assets/Features/WorkerAI/Scripts/Pathfinding/AIPathAlignedToPlanetSurface.cs
@@ -9,23 +9,14 @@ namespace Features.WorkerAI.Scripts.Pathfinding
 {
     public class AIPathAlignedToPlanetSurface : AIPathAlignedToSurface
     {
-        // private Vector3 lastSimulatedPosition;
-        private RaycastHit lastEvaluatedRaycastHit;
         public string status;
         public string lastStatus;
-
-        // protected override void Start()
-        // {
-        //     base.Start();
-        //
-        //     this.lastSimulatedPosition = this.transform.position;
-        // }
 
         private void UpdateStatus(string newStatus, Object extraInfo = null)
         {
             if (newStatus == status) return; // no change
             if (lastStatus == newStatus &&
-                status.Contains("falling") && 
+                status.Contains("falling") &&
                 newStatus.StartsWith("left")) return; // falling loop
 
             lastStatus = status;
@@ -33,65 +24,50 @@ namespace Features.WorkerAI.Scripts.Pathfinding
             Debug.Log("Agent is " + newStatus + (extraInfo == null ? string.Empty : " " + extraInfo));
         }
 
-        protected override void UpdateMovementPlane () {
-            if (prevPosition2 == position)
-            {
-                UpdateStatus("100% not moving");
-                // Agent hasn't moved, so it can't be falling
-                return;
-            }
-            if (prevPosition2.EqualEnough(position, Vector3.kEpsilon))
+        public Vector3 desiredMovement => lastDeltaTime > 0.00001f
+            ? movementPlane.ToWorld(lastDeltaPosition)
+            : Vector3.zero;
+
+        protected override void UpdateMovementPlane()
+        {
+            if (prevPosition2.EqualEnough(position, 1E-04f))
             {
                 UpdateStatus("not moving enough", (prevPosition2 - position));
                 // Agent hasn't moved, so it can't be falling
                 return;
             }
 
-            // lastSimulatedPosition = simulatedPosition;
-
-            // if (lastEvaluatedRaycastHit.point != Vector3.zero &&
-            //     lastEvaluatedRaycastHit.point == lastRaycastHit.point)
             if (lastRaycastHit.point == Vector3.zero)
             {
-                // Seems like the lastRaycastHit is still the same
+                // lastRaycastHit didn't hit anything
                 // --> no ground was found this update
                 // --> agent is falling
-                UpdateStatus("Left the movement plane, try to grab onto the next wall." , (prevPosition2 - position));
-                // float elevation;
-                //
-                // movementPlane.ToPlane(position, out elevation);
+                UpdateStatus("Left the movement plane, try to grab onto the next wall.", (prevPosition2 - position));
                 // Half height of the agent, downwards
                 Vector3 rayOffset = movementPlane.ToWorld(Vector2.zero, tr.localScale.y * height * -0.5f);
-                Debug.DrawLine(position, position+rayOffset, Color.blue, 30);
+                Debug.DrawLine(position, position + rayOffset, Color.blue, 30);
                 var tangent = new Vector3();
                 Vector3.OrthoNormalize(ref rayOffset, ref tangent);
-                Debug.DrawLine(position+rayOffset, position+rayOffset + tangent, Color.magenta, 30);
+                Debug.DrawLine(position + rayOffset, position + rayOffset + tangent, Color.magenta, 30);
 
                 // Check all 4 directions for a possible wall to grab
+                // TODO only use invert (plane) movement
                 float shortestDistance = Single.PositiveInfinity;
-                // RaycastHit closestCubeHit;
                 for (int i = 0; i < 4; i++)
                 {
-                    var direction = /*position
-                                    + */Quaternion.AngleAxis(90 * i, rayOffset)
-                                    * tangent;
-                    // Debug.Log(direction);
-                    Debug.DrawLine(position+rayOffset, position+rayOffset + (direction * (maxSpeed * 2)), Color.green, 30);
-                    if (UnityEngine.Physics.Raycast(
+                    var direction = Quaternion.AngleAxis(90 * i, rayOffset) * tangent;
+                    Debug.DrawLine(position + rayOffset, position + rayOffset + (direction * (maxSpeed * 2)),
+                        Color.green, 30);
+                    if (!UnityEngine.Physics.Raycast(
                             position + rayOffset,
                             direction,
                             out var cubeHit,
                             maxSpeed * 2,
                             groundMask,
-                            QueryTriggerInteraction.Ignore))
-                    {
-                        if (cubeHit.distance < shortestDistance)
-                        {
-                            shortestDistance = cubeHit.distance;
-                            lastRaycastHit = cubeHit;
-                            // closestCubeHit = cubeHit;
-                        }
-                    }
+                            QueryTriggerInteraction.Ignore)) continue;
+                    if (!(cubeHit.distance < shortestDistance)) continue;
+                    shortestDistance = cubeHit.distance;
+                    lastRaycastHit = cubeHit;
                 }
 
                 if (float.IsPositiveInfinity(shortestDistance))
@@ -101,24 +77,48 @@ namespace Features.WorkerAI.Scripts.Pathfinding
                     UpdateStatus("falling aaaaAAAAAHHHHHH");
                 }
             }
+            // Check for inner corners/edges
+            // e.g. from a higher cube onto an orthogonal lower cube
             else
             {
-                UpdateStatus("grounded");
+                var desiredV = desiredMovement;
+                Vector3 rayOffset = movementPlane.ToWorld(Vector2.zero, tr.localScale.y * height * 0.5f);
+                Debug.DrawRay(position, rayOffset, Color.blue, 30);
+                var dir = desiredV;
+                var maxDistance = tr.localScale.x * radius + desiredV.magnitude * 2;
+                dir *= (maxDistance) / dir.magnitude;
+                Debug.DrawRay(position + rayOffset - desiredV, dir, Color.red, 30);
+                if (UnityEngine.Physics.Raycast(
+                        position + rayOffset - desiredV, // start from negative desired velocity to ensure we didn't miss the cube wall
+                        desiredV, // TODO remove verticalVelocity?
+                        out var cubeHit,
+                        maxDistance, // Look ahead the expected future position
+                        groundMask,
+                        QueryTriggerInteraction.Ignore))
+                {
+                    UpdateStatus("Moving onto orthogonal wall.");
+                    lastRaycastHit = cubeHit;
+                }
+                else
+                {
+                    UpdateStatus("grounded");
+                }
             }
-            // lastEvaluatedRaycastHit = lastRaycastHit;
-            
-            // TODO how to walk across inner corners (e.g. from a higher cube onto an orthogonal lower cube)
-            
-            // base.UpdateMovementPlane();
+
             var normal = lastRaycastHit.normal;
             if (normal == Vector3.right)
             {
+                // TODO FIXME this prevents fwd to be (0,0,0) if
+                // movementPlane.rotation = (0,0,0)
                 normal = new Vector3(1, 0.01f, 0);
             }
-            if (normal != Vector3.zero) {
+
+            if (normal != Vector3.zero)
+            {
                 var fwd = Vector3.Cross(movementPlane.rotation * Vector3.right, normal);
                 movementPlane = new SimpleMovementPlane(Quaternion.LookRotation(fwd, normal));
             }
+
             if (rvoController != null) rvoController.movementPlane = movementPlane;
         }
     }
